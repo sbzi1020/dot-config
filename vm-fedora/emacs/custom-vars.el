c10ad7e647327383a41ad6cf9c642a0118a4f152
Fion <li.shangzi3@gmail.com>
2024-01-21 11:06:19 +1300

    [master] - vm-fedora, emacs config

---
+++ vm-fedora/emacs/custom-vars.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(package-selected-packages
+   '(denote org-noter zig-mode yasnippet xclip which-key vertico rust-mode pdf-tools org-roam org-download org-bullets org-auto-tangle olivetti markdown-mode marginalia highlight-indent-guides helpful fish-mode evil-surround evil-nerd-commenter evil-goggles evil-collection embark-consult doom-themes company command-log-mode all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
