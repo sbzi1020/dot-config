aeb78cba3eeabee951cb0c9905cf83643959571b
Fion Li <li.shangzi3@gmail.com>
2024-07-06 21:33:04 +1200

    [ master ] - asahi-linux, updated waybar configuration.

--- asahi-linux/dot_configs/waybar/scripts/disk-usage.sh
+++ asahi-linux/dot_configs/waybar/scripts/disk-usage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 
-set --local disk_info (df -Th | rg ext4 | awk '{print $3,$4,$5}')
+set --local disk_info (df -Th | rg btrfs | awk '{print $3,$4,$5}')
 set --local total (echo "$disk_info" | cut -d ' ' -f1)
 set --local used (echo "$disk_info" | cut -d ' ' -f2)
 set --local left (echo "$disk_info" | cut -d ' ' -f3)
--- asahi-linux/dot_configs/waybar/scripts/toggle-mute.sh
+++ asahi-linux/dot_configs/waybar/scripts/toggle-mute.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/fish
 set --local DEVICE_ID (wpctl status | rg "HDMI" | head -n 1 | awk '{print $3}' | sed -e "s/\.//g")
-# echo "DEVICE_ID: $DEVICE_ID"
+echo "DEVICE_ID: $DEVICE_ID"
 wpctl set-mute $DEVICE_ID toggle
