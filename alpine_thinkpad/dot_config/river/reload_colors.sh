afe4c9a3bfa94b265fd9a481519505e443a5d7c0
FionLi <li.shangzi3@gmail.com>
2025-12-24 10:57:38 +1300

    [ master ] - alpine_thinkpad, update river configuration.

---
+++ alpine_thinkpad/dot_config/river/reload_colors.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+
+source ~/.cache/wal/colors-river
+
+riverctl background-color $river_background
+riverctl border-color-focused $river_color1
+riverctl border-color-unfocused 0x595959aa
